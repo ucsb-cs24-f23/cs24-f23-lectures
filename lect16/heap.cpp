0ba300f0df1560e76faf064dc8d32df2057e0784
dibamirza <dimirza@cs.ucsb.edu>
2023-11-29 16:02:34 -0800

    lect 16

--- lect16/heap.cpp
+++ lect16/heap.cpp
@@ -7,6 +7,7 @@
 #include <queue>
 #include <time.h>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -60,21 +61,81 @@ void simple_heap_sort(vector<int>& v){
 // overall: running time : O(n log n)
 // Space complexity: O(n)
 
+void bubble_down(vector<int> & v, int i, int size){
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+    int max_index = i;
+    int n = size;
+    cout << "-";
+    if(left < n){
+        if(v[left] > v[max_index]){
+            max_index= left;
+        }
+    }
+    if(right < n){
+        if(v[right] > v[max_index]){
+            max_index= right;
+        }
+    }
+    if(max_index != i){
+        swap(v[i], v[max_index]);
+        bubble_down(v, max_index, n);
+    }
 
+}
+
+
+
+void heapify_v0(vector<int> & v){
+   // converts vector into a max heap
+    int n = v.size(); 
+    for(int i = (n - 1) ; i >= 0; i--){
+        bubble_down(v, i, n);
+    }
+}
+
+void heapify(vector<int> & v){
+   // converts vector into a max heap
+    int n = v.size(); 
+    int h = log2(n); //height of the heap
+   // cout << "height = " << h << endl;
+    for(int l = (h - 1) ; l >= 0; l--){ // h
+        cout <<" level = "<< l <<": ";
+        for(int i = pow(2, l + 1) - 2 ; i >= pow(2, l) - 1 ; i--){ // 2 ^ l
+           bubble_down(v, i, n); // h - l
+           cout << "|" ;
+        }
+        cout << endl;
+    }
+}
+
+void heap_sort(vector<int> & v){
+    heapify(v);
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        swap(v[0], v[i]);
+        // reduce the size of the heap by 1
+        bubble_down(v, 0, i);
+    }   
+}
 
 int main(int argc, char const *argv[])
 {
    // srand(time(NULL));
     vector<int> v {8, 50, 74, 59, 31, 73, 45, 79, 24, 10, 41, 66, 93, 43, 88, 4, 28, 30, 41, 13, 4, 70, 10, 58, 61, 34, 100, 79, 17, 36};
-    // Another example {1, 5, 3, 6, 4, 1, 7, 8, 4}
-    // for(int i = 0; i< 30; i++){
-    //     v.push_back(rand() % 100 + 1);
-    // }
-    cout<<"Before"<<endl;
-    visualize(v);
-    simple_heap_sort(v);
-    cout<<"After sorting"<<endl;
-    visualize(v);
+    //vector<int> v  {1, 5, 3, 6, 4, 1, 7, 8, 4};
+    for(int i = 0; i< 30; i++){
+        v.push_back(rand() % 100 + 1);
+    }
+    // cout<<"Before"<<endl;
+    // visualize(v);
+    heapify(v);
 
+    // for(auto e : v){
+    //     cout << e << " ";
+    // }
+    // cout << endl;
+    // cout<<"After sorting"<<endl;
+    // visualize(v);
     return 0;
 }
